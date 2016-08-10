f0139948ef84a9a8946874659334fb1c85e9cff4
Hyeokreal <Hyeokryeol.yang@gmail.com>
2016-08-11 04:48:06 +0900

    pulse included bluetooth

---
+++ bluetooth/bluetooth/bluetooth.ino
@@ -0,0 +1,23 @@
+#include "SoftwareSerial.h"
+
+SoftwareSerial btSerial(2,3);
+
+void setup() {
+  // put your setup code here, to run once:
+    Serial.begin(9600);
+    btSerial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if(Serial.available()){
+    btSerial.write(Serial.read()); 
+  }
+
+  if(btSerial.available()){
+    Serial.write(btSerial.read());
+  }
+
+}
